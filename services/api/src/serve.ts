5ed0fd2219ae7f93994dc4391edd5ddb3437831a
Isx <isflogar0103@gmail.com>
2021-11-26 21:15:55 -0500

    .

--- services/api/src/serve.ts
+++ services/api/src/serve.ts
@@ -3,12 +3,11 @@ import express from "express";
 import UserRoutes from "./routes/UserRoutes";
 
 const app = express();
-const port = 8080;
 
 // define a route handler for the default home page
 app.use("/", UserRoutes());
 
 // start the Express server
-app.listen( port, () => {
+app.listen(  process.env.NODE_PORT, () => {
     console.log( `enviroment ${ process.env.NODE_ENV }, server started at http://localhost:${ process.env.NODE_PORT }` );
 });
